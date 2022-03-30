ecfb750ec28541be9150849375dd13ba23eff517
alex-killer <onefrank@bk.ru>
2022-03-30 22:33:51 +0700

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        view()->composer('includes.sidebar', function ($view){
+            $view->with('archives', Post::archives());
+    });
+
         Schema::defaultStringLength(191);
     }
 }
